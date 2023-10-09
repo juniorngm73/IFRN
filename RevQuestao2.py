9ff6582371007678bc9ffbc1657e0f90f6c06466
juniorngm73 <juniorngm@yahoo.com.br>
2023-10-08 23:19:00 -0300

    Update RevQuestao2.py

--- RevQuestao2.py
+++ RevQuestao2.py
@@ -1,13 +1,24 @@
+
+import os
 def ler_arquivo():
+    while True:
+        try:
 
-    nome_arquivo = input('Digite o Arquivo a ser Lido : ')
-    arquivo = open(f'{nome_arquivo}.txt', 'r')
-    conteudo = arquivo.read()
+            nome_arquivo = input('Digite o Arquivo a ser Lido : ')
+            arquivo = open(f'{nome_arquivo}.txt', 'r')
+            conteudo = arquivo.read()
 
-    print(conteudo)
+            print(os.path.exists(f'./{nome_arquivo}.txt'))
 
+            print(conteudo)
 
-    arquivo.close()
+            break
+            arquivo.closecu()
+
+        except:
+            print(False)
+            print('Arquivo não Encontrado, tente novamente')
+
 
 
 ler_arquivo()
